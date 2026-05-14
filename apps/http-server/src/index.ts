9f38a3819e053ced13ba9eca9e927d7f2d771131
Piyush Singh <94911177+singhpiyush55@users.noreply.github.com>
2026-05-14 12:23:47 +0530

    fixed prisma

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,3 @@
+// add express
+// add health check endpoint
+// add a user signup endpoint that creates a new user in the database usinfg the prisma client
--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,1 +1,4 @@
 // create a new prisma client instance and export it for use in the rest of the application
+import { PrismaClient } from "@prisma/client";
+
+export const Client = new PrismaClient();
