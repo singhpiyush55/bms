677f8575afbff372ecad0fcca8695a769a65ca7f
Piyush Singh <94911177+singhpiyush55@users.noreply.github.com>
2026-05-14 12:44:31 +0530

    sync prod with main

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
+// Fetch the user from the db and display the username on the page
+
 import {Client}  from "@repo/db/client";
 
 
-export default function Home() {
+export default async function Home() {
+  // Fetch the any first user from the database
+  const user = await Client.user.findFirst();
+
   return (
     <div>
+      <h1>User Information</h1>
+      <p>Username: {user?.username}</p>
     </div>
   );
 }
