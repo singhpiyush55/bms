887b6bf3eee89fea6f3253ea0680c5a2eceb3a80
Piyush Singh <94911177+singhpiyush55@users.noreply.github.com>
2026-05-14 12:39:10 +0530

    basic setup for user signup and disply

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,3 +1,32 @@
 // add express
 // add health check endpoint
 // add a user signup endpoint that creates a new user in the database usinfg the prisma client
+
+import express from "express";
+import {Client}  from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required");
+  }
+
+  try {
+    const user = await Client.user.create({
+      data: {
+        username,
+        password
+      }
+    });
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send("Error creating user");
+  }
+});
